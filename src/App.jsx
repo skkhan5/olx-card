54343db03e1fbd5426689be0017b06cebeb6aef8
skkhan5 <yousafzai5588@gmail.com>
2024-09-11 02:34:53 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+
+import Card from "./components/Card"
+
+function App() {
+  return (
+    <>
+
+
+
+
+
+
+     <div className="flex justify-center flex-row gap-5 flex-wrap">
+
+     <Card src="vv.jpeg" title="RangRover" description="Best Car in the World"/>
+     <Card src="g.webp" title="SMARTPHONE INTEGRATION" description="Enjoy endless wireless infotainment at your fingertips with the latest iDrive Operating System. Safely and easily access your favourite apps with Apple CarPlay™ and BMW Intelligent Personal Assistant at your beck and call."/>
+     <Card src="ff.webp" title="EXTERIOR" description="Dazzlingly bright and distinctive: state-of-the-art LEDs in the front headlights, rear lights, daytime driving lights and position lights shine when the vehicle is stationary and driving."/>
+     <Card src="ffg.webp" title="INTERIOR" description="With the wireless charging tray providing extremely convenient wireless smartphone charging and an extended storage area within reach, there's plenty of room for larger devices without annoying cables. The BMW head-up display further enhances your driving experience by projecting navigation and driving information directly into your field of view, allowing you to concentrate fully on the road. Simply control your music, navigation and vehicle settings using the large curved display and the BMW Live Cockpit Professional with Operating System 9."/>
+     <Card src="uu.webp" title="POWERFULLY COME TO A STOP." description="Four tailpipes create a distinct M exhaust sound."/>
+     <Card src="55.webp" title="FEELS LIKE AN M" description="Your style perfectly coordinated. Powerful engine and sporty driving dynamics. The new BMW X2 M35i xDrive M TwinPower Turbo inline 4-cylinder engine delivers up to 312 horsepower."/>
+     
+     </div>
+    </>
+  )
+}
+
+export default App
